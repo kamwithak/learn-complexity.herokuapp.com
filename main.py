662281431477d87d570d948bc1d28e8d5dd83ba5
an0n <theblizzzzard@gmail.com>
2021-02-05 02:09:18 -0500

    NICE

--- main.py
+++ main.py
@@ -99,67 +99,7 @@ def welcome():
 
   return render_template('welcome.html')
 
-@app.route('/login')
-def login():
-  google_provider_cfg = get_google_provider_cfg()
-  authorization_endpoint = google_provider_cfg["authorization_endpoint"]
-
-  request_uri = client.prepare_request_uri(
-      authorization_endpoint,
-      redirect_uri=request.base_url + "/callback",
-      scope=["openid", "email", "profile"],
-  )
-
-  return redirect(request_uri)
-
-@app.route("/logout")
-@login_required
-def logout():
-  logout_user()
-  return redirect(url_for("main"))
-
-@app.route("/login/callback")
-def callback():
-  code = request.args.get("code")
-  google_provider_cfg = get_google_provider_cfg()
-  token_endpoint = google_provider_cfg["token_endpoint"]
-  token_url, headers, body = client.prepare_token_request(
-      token_endpoint,
-      authorization_response=request.url,
-      redirect_url=request.base_url,
-      code=code
-  )
-  token_response = requests.post(
-      token_url,
-      headers=headers,
-      data=body,
-      auth=('690367163720-oj3s93qbh35cme82rakctnqh9ifv9671', 'GkqZnHkr4lreCF7vSu0mpSo8')
-  )
-  client.parse_request_body_response(json.dumps(token_response.json()))
-  userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
-  uri, headers, body = client.add_token(userinfo_endpoint)
-  userinfo_response = requests.get(uri, headers=headers, data=body)
-  if userinfo_response.json().get("email_verified"):
-    unique_id = userinfo_response.json()["sub"]
-    users_email = userinfo_response.json()["email"]
-    picture = userinfo_response.json()["picture"]
-    users_name = userinfo_response.json()["given_name"]
-  else:
-    return "User email not available or not verified by Google.", 400
-  
-  user = User(
-    id_=unique_id, name=users_name, email=users_email, profile_pic=picture
-  )
-
-  if not User.get(unique_id):
-    User.create(unique_id, users_name, users_email, picture)
-
-  login_user(user)
-
-  return redirect(url_for("main"))
-
 @app.route('/fundamentals')
-@login_required
 def fundamentals():
   agent = UserAgent(request.headers.get('User-Agent'))
 
@@ -219,6 +159,65 @@ def quiz_answers():
       return render_template('failure.html')
   else:
     return redirect("/problems")
+
+@app.route('/login')
+def login():
+  google_provider_cfg = get_google_provider_cfg()
+  authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+  request_uri = client.prepare_request_uri(
+      authorization_endpoint,
+      redirect_uri=request.base_url + "/callback",
+      scope=["openid", "email", "profile"],
+  )
+
+  return redirect(request_uri)
+
+@app.route("/login/callback")
+def callback():
+  code = request.args.get("code")
+  google_provider_cfg = get_google_provider_cfg()
+  token_endpoint = google_provider_cfg["token_endpoint"]
+  token_url, headers, body = client.prepare_token_request(
+      token_endpoint,
+      authorization_response=request.url,
+      redirect_url=request.base_url,
+      code=code
+  )
+  token_response = requests.post(
+      token_url,
+      headers=headers,
+      data=body,
+      auth=('690367163720-oj3s93qbh35cme82rakctnqh9ifv9671', 'GkqZnHkr4lreCF7vSu0mpSo8')
+  )
+  client.parse_request_body_response(json.dumps(token_response.json()))
+  userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+  uri, headers, body = client.add_token(userinfo_endpoint)
+  userinfo_response = requests.get(uri, headers=headers, data=body)
+  if userinfo_response.json().get("email_verified"):
+    unique_id = userinfo_response.json()["sub"]
+    users_email = userinfo_response.json()["email"]
+    picture = userinfo_response.json()["picture"]
+    users_name = userinfo_response.json()["given_name"]
+  else:
+    return "User email not available or not verified by Google.", 400
+  
+  user = User(
+    id_=unique_id, name=users_name, email=users_email, profile_pic=picture
+  )
+
+  if not User.get(unique_id):
+    User.create(unique_id, users_name, users_email, picture)
+
+  login_user(user)
+
+  return redirect(url_for("main"))
+
+@app.route("/logout")
+@login_required
+def logout():
+  logout_user()
+  return redirect(url_for("main"))
 
 if __name__ == '__main__':
   app.run(debug=True)
