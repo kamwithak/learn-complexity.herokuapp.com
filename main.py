1cc3469b91acc668713423a6e9a5eed5c6a76c13
an0n <theblizzzzard@gmail.com>
2021-02-04 22:48:04 -0500

    NEW: nice

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from flask_login import (
     LoginManager,
     current_user,
     # login_required,
-    # login_user,
+    login_user,
     # logout_user,
 )
 import requests
 from oauthlib.oauth2 import WebApplicationClient
 # from db import init_db_command
 from user import User
-# import json
+import json
 # import sqlite3
 
 app = Flask(__name__)
@@ -96,13 +96,73 @@ def welcome():
   if (agent.platform in ['blackberry', 'android', 'iphone', 'ipad']):
     message = f'<h1>Your {agent.platform} device is currently unsupported⏰<br> Please access LearnComplexity.io from a computer 🖥️</h1>'
     return message
+  
+  google_provider_cfg = get_google_provider_cfg()
+  authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+  # Use library to construct the request for Google login and provide
+  # scopes that let you retrieve user's profile from Google
+  request_uri = client.prepare_request_uri(
+      authorization_endpoint,
+      redirect_uri=request.base_url + "/callback",
+      scope=["openid", "email", "profile"],
+  )
+
   return render_template('welcome.html')
 
-# @app.route("/welcome/callback")
-# def callback():
-#     # Get authorization code Google sent back to you
-#     code = request.args.get("code")
-#     return f'<h1>{code}</h1>'
+@app.route("/welcome/callback")
+def callback():
+  # Get authorization code Google sent back to you
+  code = request.args.get("code")
+  # Find out what URL to hit to get tokens that allow you to ask for
+  # things on behalf of a user
+  google_provider_cfg = get_google_provider_cfg()
+  token_endpoint = google_provider_cfg["token_endpoint"]
+  # Prepare and send a request to get tokens! Yay tokens!
+  token_url, headers, body = client.prepare_token_request(
+      token_endpoint,
+      authorization_response=request.url,
+      redirect_url=request.base_url,
+      code=code
+  )
+  token_response = requests.post(
+      token_url,
+      headers=headers,
+      data=body,
+      auth=('690367163720-oj3s93qbh35cme82rakctnqh9ifv9671', 'GkqZnHkr4lreCF7vSu0mpSo8')
+  )
+  client.parse_request_body_response(json.dumps(token_response.json()))
+  # Now that you have tokens (yay) let's find and hit the URL
+  # from Google that gives you the user's profile information,
+  # including their Google profile image and email
+  userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+  uri, headers, body = client.add_token(userinfo_endpoint)
+  userinfo_response = requests.get(uri, headers=headers, data=body)
+
+  # You want to make sure their email is verified.
+  # The user authenticated with Google, authorized your
+  # app, and now you've verified their email through Google!
+  if userinfo_response.json().get("email_verified"):
+    unique_id = userinfo_response.json()["sub"]
+    users_email = userinfo_response.json()["email"]
+    picture = userinfo_response.json()["picture"]
+    users_name = userinfo_response.json()["given_name"]
+  else:
+    return "User email not available or not verified by Google.", 400
+  
+  user = User(
+    id_=unique_id, name=users_name, email=users_email, profile_pic=picture
+  )
+
+  # Doesn't exist? Add it to the database.
+  if not User.get(unique_id):
+    User.create(unique_id, users_name, users_email, picture)
+
+  # Begin user session by logging the user in
+  login_user(user)
+
+  # Send user back to homepage
+  return redirect(url_for("main"))
 
 @app.route('/fundamentals')
 def fundamentals():
