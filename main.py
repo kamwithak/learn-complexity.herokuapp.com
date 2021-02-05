7bd6e478e8f8894b78c56754a420a5aa089c9639
an0n <theblizzzzard@gmail.com>
2021-02-05 18:23:44 -0500

    NICE

--- main.py
+++ main.py
@@ -231,5 +231,9 @@ if __name__ == '__main__':
   # gunicorn_logger = logging.getLogger('gunicorn.error')
   # app.logger.handlers = gunicorn_logger.handlers
   # app.logger.setLevel(gunicorn_logger.level)
-  context = ('ssl/cert.pem', 'ssl/key.pem')
+  # context = ('ssl/cert.pem', 'ssl/key.pem')
+  from OpenSSL import SSL
+  context = SSL.Context(SSL.PROTOCOL_TLSv1_2)
+  context.use_privatekey_file('ssl/key.pem')
+  context.use_certificate_file('ssl/cert.pem')
   app.run(ssl_context=context)
