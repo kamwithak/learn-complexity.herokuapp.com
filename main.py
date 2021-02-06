681f9e02d944a85fcff44bc1dee662d25145be8b
an0n <theblizzzzard@gmail.com>
2021-02-05 19:16:58 -0500

    NICE

--- main.py
+++ main.py
@@ -230,5 +230,5 @@ if __name__ == '__main__':
   gunicorn_logger = logging.getLogger('gunicorn.error')
   app.logger.handlers = gunicorn_logger.handlers
   app.logger.setLevel(gunicorn_logger.level)
-  
+  app.logger.info("info")
   app.run()
